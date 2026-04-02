c05b62fd0a45cac03a79d5346a57097d40263ca7
KaizokuKitsune <awilley@wheatlandhigh.org>
2026-04-02 17:15:18 +0000

    Added part 2 and step file was editied

--- Week 1/With_HW.py
+++ Week 1/With_HW.py
@@ -4,7 +4,7 @@
 # Time Spent: 
 
 
-# V1
+# Part 1
 days = int(input("Enter the number of days you wish to track the steps for: ")) # Get number of days from user
 steps_file = open("Week 1/steps.txt","w") # open a text file in write mode, needed to specify a file path of "Week 1/" to get it to be created in the file directory correctly
 for day in range(1,days+1): # for loop to iterate upon user input, use range 1 to input + 1 b/c I want the message to contain the day number
@@ -13,3 +13,11 @@ for day in range(1,days+1): # for loop to iterate upon user input, use range 1 t
 steps_file.close() # close the file for no mem leaks/other errors from leaving file open
   
   
+# Part 2
+steps_file = open("Week 1/steps.txt","r") # opens the file in read mode
+read_day = steps_file.readline() # reads the first line of the step file
+total_steps = 0 # initializes a sum value
+while read_day != '': # While line is not blank
+  total_steps += int(read_day) # converts the line to int and adds it to the sum
+  read_day = steps_file.readline()
+print(f'''The total steps tracked is {total_steps}''')
