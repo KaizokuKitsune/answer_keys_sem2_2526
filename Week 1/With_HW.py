5e96143cc4f40ad862dba821d9bfd7339e773e78
KaizokuKitsune <awilley@wheatlandhigh.org>
2026-04-02 17:08:47 +0000

    Ran a test and updated the file directory in the open statement to get it to create where I wanted it to

--- Week 1/With_HW.py
+++ Week 1/With_HW.py
@@ -6,7 +6,7 @@
 
 # V1
 days = int(input("Enter the number of days you wish to track the steps for: ")) # Get number of days from user
-steps_file = open("steps.txt","w") # open a text file in write mode
+steps_file = open("Week 1/steps.txt","w") # open a text file in write mode, needed to specify a file path of "Week 1/" to get it to be created in the file directory correctly
 for day in range(1,days+1): # for loop to iterate upon user input, use range 1 to input + 1 b/c I want the message to contain the day number
   steps = input(f'''Day #{day} | How many steps did you take? ''') # get input for steps taken on day, uses f string to include day number
   steps_file.write(f'{steps}\n') # use f string for easy/clean writing of value with newline character (\n)
